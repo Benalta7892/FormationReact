e65755ae541258d4cdbe489af97c77632f4ac4ca
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-11-18 15:42:53 +0100

    Les formulaires avec React

--- 04_FormulairesReact/src/App.jsx
+++ 04_FormulairesReact/src/App.jsx
@@ -1,5 +1,24 @@
+import { useState } from "react";
+
 function App() {
-  return "Bonjour les gens";
+  const [firstname, setFirstname] = useState("John Doe");
+
+  const handleChange = (e) => {
+    setFirstname(e.target.value);
+  };
+
+  const reset = () => {
+    setFirstname("");
+  };
+
+  return (
+    <form>
+      <input type="text" name="firstname" value={firstname} onChange={handleChange} /> {firstname}
+      <button onClick={reset} type="button">
+        Reset
+      </button>
+    </form>
+  );
 }
 
 export default App;
