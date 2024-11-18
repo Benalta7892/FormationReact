f9bb85cc0e3f3a35b974dc77ad961790e5e397c1
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-11-18 14:46:49 +0100

    Syntaxe JSX

--- 02_SyntaxeJSX/src/App.jsx
+++ 02_SyntaxeJSX/src/App.jsx
@@ -2,26 +2,34 @@
 
 const title = "Bonjour les <strong>gens</strong>";
 const style = { color: "red", backgroundColor: "blue" };
+const showTitle = false;
+const todos = ["Présenter react", "Présenter le JSX", "Crèer des composants"];
 
 function App() {
-  const handleClick = (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    alert("J'ai  sur le titre");
-  };
-
   return (
     <>
-      <h1 onClick={handleClick} id="title" className="title" style={style}>
-        {title}
-      </h1>
+      <Title color="blue">Mon composant</Title>
       <input type="text" />
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati dicta est nesciunt facilis hic sunt eligendi
         beatae et quia. Tempora, iusto quos nesciunt dolores recusandae quasi cum dolorem quae eum.
       </p>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo}>{todo}</li>
+        ))}
+      </ul>
     </>
   );
+}
+
+function Title({ color, children, hidden }) {
+  if (hidden) {
+    return null;
+  }
+  console.log(hidden);
+
+  return <h1 style={{ color: color }}>{children}</h1>;
 }
 
 export default App;
