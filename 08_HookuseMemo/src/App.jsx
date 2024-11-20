9897d7684e9bdd7245b2e64beda552def99a0256
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-11-20 13:57:20 +0100

    Le hook useMemo

--- 08_HookuseMemo/src/App.jsx
+++ 08_HookuseMemo/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { useId, useState, useMemo } from "react";
 import { Input } from "./components/forms/Input.jsx";
 
 function App() {
   const [firstname, setFirstname] = useState("John");
   const [password, setPassword] = useState("MotDePasse");
   const security = passwordSecurity(password);
+  const random = useMemo(() => Math.random(), []);
 
   return (
     <div className="container my-3 vstack gap-2">
+      {random}
       <Input label="Nom d'utilisateur" value={firstname} onChange={setFirstname} />
       <div>
         <Input label="Password" value={password} onChange={setPassword} />
@@ -18,7 +20,13 @@ function App() {
 }
 
 function passwordSecurity(password) {
-  if (password.length < 4) {
+  // Fausse lenteur
+  let startTime = performance.now();
+  while (performance.now() - startTime < 200) {
+    // Attendre 200ms
+  }
+
+  if (password.length < 3) {
     return "Faible";
   }
   if (password.length < 6) {
--- 08_HookuseMemo/src/components/forms/Input.jsx
+++ 08_HookuseMemo/src/components/forms/Input.jsx
@@ -1,8 +1,25 @@
-export function Input({ value, onChange, label, id }) {
+import { useId } from "react";
+
+/**
+ * @param {string} placeholder
+ * @param {string} value
+ * @param {string} label
+ * @param {(s: string) => void} onChange
+ */
+export function Input({ placeholder, value, onChange, label }) {
+  const id = useId();
   return (
     <div className="form-group">
-      <label>{label}</label>
-      <input value={value} className="form-control" onChange={(e) => onChange(e.target.ed)} />
+      <label className="form-label" htlmfor={id}>
+        {label}
+      </label>
+      <input
+        id={id}
+        className="form-control"
+        value={value}
+        placeholder={placeholder}
+        onChange={(e) => onChange(e.target.value)}
+      />
     </div>
   );
 }
