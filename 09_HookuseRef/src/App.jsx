2d94c13f70a780eda7ef187df929fc20877927d5
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-11-20 14:14:15 +0100

    Le hook useRef

--- 09_HookuseRef/src/App.jsx
+++ 09_HookuseRef/src/App.jsx
@@ -1,5 +1,20 @@
+import { useId, useState, useRef, useEffect, useMemo } from "react";
+import { Input } from "./components/forms/Input.jsx";
+
 function App() {
-  return "Salut les gens";
+  const ref = useRef(null);
+
+  useEffect(() => {
+    console.log(ref.current.offsetHeight);
+  }, []);
+
+  return (
+    <div ref={ref}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste quae laboriosam officia rem eos a? Necessitatibus
+      similique culpa tempore inventore voluptatibus, recusandae, provident ea saepe totam magnam blanditiis fugiat
+      ipsa!
+    </div>
+  );
 }
 
 export default App;
