e830a4323f014993ff487fa301c9da6595c5ebfd
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-11-21 11:17:14 +0100

    Creer un hook personnalise

--- 10_HookPersonnalise/src/App.jsx
+++ 10_HookPersonnalise/src/App.jsx
@@ -1,25 +1,29 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useIncrement } from "./hooks/useIncrement.js";
 import { useDocumentTitle } from "./hooks/useDocumentTitle.js";
 import { Input } from "./components/forms/Input.jsx";
+import { useFetch } from "./hooks/useFetch.js";
 
 function App() {
-  const { count, increment, decrement } = useIncrement({
-    base: 0,
-    max: 10,
-    min: 0,
-  });
-
-  const [name, setName] = useState("");
-
-  useDocumentTitle(name ? "Editer " + name : null);
+  const { loading, data, errors } = useFetch("https://jsonplaceholder.typicode.com/posts?_limit=11&_delay=2000");
 
   return (
-    <div>
-      <Input value={name} onChange={setName} label="Nom" />
-      Compteur {count}
-      <button onClick={increment}>Incrémenter</button>
-      <button onClick={decrement}>Décrémenter</button>
+    <div className="container my-2">
+      {loading && (
+        <div className="spinner-border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </div>
+      )}
+      {errors && <div className="alert alert-danger">{errors.toString()}</div>}
+      {data && (
+        <div>
+          <ul>
+            {data.map((post) => (
+              <li key={post.id}>{post.title}</li>
+            ))}
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
