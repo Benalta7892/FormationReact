a801d57d67c93e20c5c734f481fb4ff6ff75d347
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-11-21 12:36:56 +0100

    Les portails

---
+++ 12_Portails/src/App.jsx
@@ -0,0 +1,34 @@
+function App() {
+  return (
+    <div
+      style={{
+        height: 300,
+        overflowY: "scroll",
+        background: "#EEE",
+        margin: 20,
+        position: "relative",
+      }}
+    >
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis delectus perspiciatis consequuntur dolor
+        repellat placeat. Velit, sapiente? Aliquid vel quam quaerat id cumque animi saepe praesentium ratione! Quisquam,
+        quo sint?
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis delectus perspiciatis consequuntur dolor
+        repellat placeat. Velit, sapiente? Aliquid vel quam quaerat id cumque animi saepe praesentium ratione! Quisquam,
+        quo sint?
+      </p>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis delectus perspiciatis consequuntur dolor
+        repellat placeat. Velit, sapiente? Aliquid vel quam quaerat id cumque animi saepe praesentium ratione! Quisquam,
+        quo sint?
+      </p>
+      <Modal />
+    </div>
+  );
+}
+
+function Modal() {}
+
+export default App;
