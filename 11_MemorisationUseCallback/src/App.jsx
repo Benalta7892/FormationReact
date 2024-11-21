f46a685f87ad8988bdca4a9adc5ff16baa161046
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-11-21 12:00:03 +0100

    Memorisation et useCallback

--- 11_MemorisationUseCallback/src/App.jsx
+++ 11_MemorisationUseCallback/src/App.jsx
@@ -1,21 +1,32 @@
 import { useState } from "react";
 import { Input } from "./components/forms/Input.jsx";
+// import { waitSync } from "./utils/waitSync.js";
 
 function App() {
-  const [name, setName] = useState("");
+  console.log("App", "render");
 
   return (
     <div className="container my-2 vstack gap-2">
-      <div>
-        <Input label="Prénom" onChange={setName} value={name} />
-        <div>{name.toUpperCase()}</div>
-      </div>
+      <Demo />
       <Info />
     </div>
   );
 }
 
+function Demo() {
+  const [name, setName] = useState("");
+
+  return (
+    <div>
+      <Input label="Prénom" onChange={setName} value={name} />
+      <div>{name.toUpperCase()}</div>
+    </div>
+  );
+}
+
 function Info() {
+  // waitSync(500);
+
   return (
     <div className="alert alert-info">
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, veniam velit? Officiis accusamus perferendis
