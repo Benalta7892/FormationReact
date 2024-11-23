0bb16bfdbfd0ddbeeda36a3ad1068cf1186da6b1
Benoit ALEXANDRE TACHDJIAN <benoit.alexandre78@gmail.com>
2024-11-23 12:53:31 +0100

    React router dom

--- 21_ReactRouterDom/src/App.jsx
+++ 21_ReactRouterDom/src/App.jsx
@@ -1,5 +1,51 @@
+import { createBrowserRouter, RouterProvider, Link, NavLink } from "react-router-dom";
+import { Single } from "./pages/Single";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div>
+        Page d'accueil
+        <nav>
+          <Link to="/blog">Blog</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+      </div>
+    ),
+  },
+  {
+    path: "/blog",
+    element: (
+      <div>
+        Blog
+        <nav>
+          <NavLink to="/blog">Blog</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </nav>
+      </div>
+    ),
+  },
+  {
+    path: "/blog/:id",
+    element: <Single />,
+  },
+  {
+    path: "/contact",
+    element: (
+      <div>
+        Contact
+        <nav>
+          <Link to="/blog">Blog</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+      </div>
+    ),
+  },
+]);
+
 function App() {
-  return "Bonjour tout le monde";
+  return <RouterProvider router={router} />;
 }
 
 export default App;
